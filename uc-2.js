671e8353c98c62d18fd3e98a9b3acf34d584e5c7
Gautamverma2003 <gautam146official@gmail.com>
2025-03-05 23:02:49 +0530

    [Gautam] add.Added Validate pin code which restrict alphabet and special character in the beginning with regex

---
+++ uc-2.js
@@ -0,0 +1,8 @@
+function validatePin(pincode){
+    //it will restrict alphabet and speacial character at the beginning
+
+    let regex = /^[1-9][0-9]{5}$/;
+    return regex.test(pincode);
+}
+//Uc02 here:
+console.log("Validating pincode " + validatePin("A40088"))
